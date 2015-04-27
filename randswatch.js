b312de1d95bd021bec2cf73990a6448cb2e4c696
phase <ph4sesaber@gmail.com>
2015-04-27 11:24:17 -0700

    Minify CSS import

--- randswatch.js
+++ randswatch.js
@@ -1,5 +1,5 @@
 $.get("http://api.bootswatch.com/3/", function(data){
   var themes = data.themes;
   var theme = themes[Math.floor(Math.random()*themes.length)];
-  $("head").append('<link rel="stylesheet" type="text/css" href="' + theme.css + '" />');
+  $("head").append('<link rel="stylesheet" type="text/css" href="' + theme.cssMin + '" />');
 });
